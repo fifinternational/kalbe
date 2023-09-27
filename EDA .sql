19ee952ec047174e9354976b9cafa347d938b8ef
fifinternational <99740757+fifinternational@users.noreply.github.com>
2023-09-27 17:02:56 +0700

    Update EDA .sql

--- EDA .sql
+++ EDA .sql
@@ -2,4 +2,8 @@ SELECT "Marital Status", AVG("age") AS "Rata-rata Umur"
 FROM customer1 c 
 GROUP BY "Marital Status";
 
+SELECT gender , AVG(age) AS AverageAge
+FROM customer_case_study
+GROUP BY gender;
+
 
